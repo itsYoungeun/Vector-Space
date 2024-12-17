527a7ffecd3952a95b2166f7c5ac5454dfc39d53
Peter <youngeunpc@yahoo.com>
2024-12-17 08:41:04 -0300

    Learning

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 class R2Vector:
-    def __init__(self, x, y):
+    def __init__(self, *, x, y):
         self.x = x
         self.y = y
-        
+
     def norm(self):
-        return (self.x**2 + self.y**2)**0.5
-        
+        return sum(val**2 for val in vars(self).values())**0.5
+
     def __str__(self):
-        return f'{self.x, self.y}'
+        return str(tuple(getattr(self, i) for i in vars(self)))
+    
 class R3Vector(R2Vector):
-    def __init__(self, x, y, z):
-        pass
-v1 = R2Vector(2, 3)
-print(v1.norm())
-print(v1)
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
+        self.z = z
+
+v1 = R2Vector(x=2, y=3)
+v2 = R3Vector(x=2, y=2, z=3)
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
